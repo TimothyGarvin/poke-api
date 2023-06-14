e08c083762eae9ebce862e8bc14a1cb698014710
Timothy Garvin <timothygarvin4@gmail.com>
2023-06-13 19:15:29 -0700

    Updated to use Axion, fetch ver. commented.

--- src/components/fetchPoke.js
+++ src/components/fetchPoke.js
@@ -1,24 +1,30 @@
 import React, {useState} from 'react'
+import axios from 'axios'
 
 const FetchPoke = (props) => {
 
     const [list, setList] = useState([])
 
     const pokeList = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
-        .then(response => {
-            return response.json();
-        })
-        .then(response => {
-            console.log(response);
-            setList(response.results)
-            console.log(list)
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    }
+    // fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+    //     .then(response => {
+    //         return response.json();
+    //     })
+    //     .then(response => {
+    //         console.log(response);
+    //         setList(response.results)
+    //         console.log(list)
+    //     })
+    //     .catch(err=>{
+    //         console.log(err)
+    //     })
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807").then(response=>{
+                console.log(response);
+                setList(response.data.results)
 
+            })
+            
+        }
     return(
         <div>
             <button onClick={pokeList}> Fetch Pokemon </button>
